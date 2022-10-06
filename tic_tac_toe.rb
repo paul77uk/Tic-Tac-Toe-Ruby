30ef42cb32a8ffacee8d34a4a347685a21b1d226
Paul Vickers <paul77uk@gmail.com>
2022-10-06 22:10:12 +0100

    fixed bug

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,5 @@
 class Player
-  ENTER_NAME_MESSAGE = " enter your name:".freeze
+  ENTER_NAME_MESSAGE = ' enter your name:'.freeze
   attr_accessor :player_num, :name, :letter
 
   def initialize(player_num, letter)
@@ -15,16 +15,16 @@ class Player
 end
 
 class Board
-  OUT_OF_RANGE_ERROR_MESSAGE = "must be a number from 1 to 9".freeze
-  SQUARE_OCCUPIED_ERROR_MESSAGE = "This square is already occupied".freeze
-  INPUT_MESSAGE = " enter a number from 1 to 9".freeze
+  OUT_OF_RANGE_ERROR_MESSAGE = 'must be a number from 1 to 9'.freeze
+  SQUARE_OCCUPIED_ERROR_MESSAGE = 'This square is already occupied'.freeze
+  INPUT_MESSAGE = ' enter a number from 1 to 9'.freeze
   DIVIDING_LINE = " -----------       -----------\n".freeze
   NUMBERED_BOARD_VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9].freeze
   WINNING_COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
   attr_accessor :board_values, :is_player_one
 
   def initialize
-    @board_values = [" "] * 9
+    @board_values = [' '] * 9
     @is_player_one = true
   end
 
@@ -39,11 +39,11 @@ class Board
   end
 
   def error_handling(input)
-    while !NUMBERED_BOARD_VALUES.include?(input) || @board_values[input - 1] != " "
-      if @board_values[input - 1] != " "
-        puts SQUARE_OCCUPIED_ERROR_MESSAGE
-      else
+    while !NUMBERED_BOARD_VALUES.include?(input) || @board_values[input - 1] != ' '
+      if !NUMBERED_BOARD_VALUES.include?(input)
         puts OUT_OF_RANGE_ERROR_MESSAGE
+      else
+        puts SQUARE_OCCUPIED_ERROR_MESSAGE
       end
       input = gets.chomp.to_i
     end
@@ -55,7 +55,7 @@ class Board
     puts player.name + INPUT_MESSAGE
     input = gets.chomp.to_i
     input = error_handling(input)
-    @board_values[input - 1] = player.letter if NUMBERED_BOARD_VALUES.include?(input)
+    @board_values[input - 1] = player.letter 
   end
 
   def content_line(index1, index2, index3)
@@ -64,9 +64,9 @@ class Board
   end
 
   def game_loop(player1, player2)
-    while @board_values.include? " "
+    while @board_values.include? ' '
       get_position_input(@is_player_one ? player1 : player2)
-      system "clear"
+      system 'clear'
       puts display
       @is_player_one = !@is_player_one
       return if game_logic(player1, @board_values)
@@ -78,8 +78,8 @@ class Board
       if winning_line(index[0], index[1], index[2], player, @board_values)
         puts "\n#{player.name} wins"
         return true
-      elsif !@board_values.include? " "
-        puts "Draw"
+      elsif !@board_values.include? ' '
+        puts 'Draw'
         return true
       end
     end
@@ -93,13 +93,13 @@ class Board
   end
 end
 
-player1 = Player.new("Player1", "X")
-player2 = Player.new("Player2", "O")
+player1 = Player.new('Player1', 'X')
+player2 = Player.new('Player2', 'O')
 board = Board.new
 
 puts
 player1.gets_player_name
 player2.gets_player_name
-system "clear"
+system 'clear'
 puts board.display
 board.game_loop(player1, player2)
